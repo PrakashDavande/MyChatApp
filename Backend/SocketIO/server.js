efcd35b7e3ac09773e35f9ea81a5b7f712cb3dd2
PrakashDavande <psdavande54@gmail.com>
2025-07-25 15:57:18 +0530

    link update

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://mychatapp-fd48.onrender.com",
+    origin: "https://mychatapp-2tkb.onrender.com",
     methods: ["GET", "POST"],
   },
 });
